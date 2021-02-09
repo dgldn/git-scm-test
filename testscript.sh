b7e9236c64af7ba2ad9b4fed86b186250e705ba5
D Giri <16837707+dgldn@users.noreply.github.com>
2021-02-09 22:44:06 +0000

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,3 +3,6 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This was added"
+
+echo "this is added later for branch test"
+echo "Mulitplication is $(($NUMBERONE * $NUMBERTWO))"
